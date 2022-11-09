e45f702f1051650e4e6d4aaf33595770399ca13f
Geraldine Diaz <geraldine.diaz@mercadolibre.com.co>
2022-11-08 19:42:54 -0500

    create item component

---
+++ src/components/ItemList.vue
@@ -0,0 +1,18 @@
+<template>
+  <li class="list__tweewp">
+    <h3 class="tweewp__title">{{ tweepTitle }}</h3>
+    <p class="tweewp__description">{{ tweepDescription }}</p>
+  </li>
+</template>
+
+<script setup>
+import { defineProps, toRefs } from "vue";
+
+const props = defineProps({
+  tweepTitle: String,
+  tweepDescription: String,
+});
+
+const { tweepTitle, tweepDescription } = toRefs(props);
+
+</script>
