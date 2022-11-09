9e090840bb67ee6222da0d6e7ecd5b8a01717d5f
Geraldine Diaz <geraldine.diaz@mercadolibre.com.co>
2022-11-08 19:43:09 -0500

    crate list component

---
+++ src/components/List.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul class="list">
+    <ItemList 
+      v-for="(item, index) in tweeps"
+      :key="index"
+      :tweepTitle="item.tweepTitle"
+      :tweepDescription="item.tweepDescription"
+    />
+    <p>Agrega un Tweep</p>
+    <input
+      type="text"
+      placeholder="escriba un título"
+      v-model="newTitle"
+    >
+    <input
+      type="text"
+      placeholder="escriba una descripción"
+      v-model="newDescription"
+    >
+    <button @click="testEjm">Enviar</button>
+  </ul>
+</template>
+
+<script setup>
+import ItemList from './ItemList.vue'
+import { reactive, ref } from 'vue'
+
+const tweeps = reactive([])
+
+const newTitle = ref('');
+const newDescription = ref('');
+
+const resetform = () => {
+  newTitle.value = ''
+  newDescription.value = ''
+}
+
+const testEjm = () => {
+  tweeps.push({
+    tweepTitle: newTitle.value,
+    tweepDescription: newDescription.value 
+  })
+  resetform()
+}
+</script>
